81c817d377d514502c2bff9a20aa89dd6b266709
Tanner Bacon <tannerbacon20@gmail.com>
2023-02-21 06:42:22 -0700

    initializing project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Group2_3_Mission8.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
