e9512aba16e832653f30bad9466c04cd69ac2d56
tx-smitht <tx.smiththomas@gmail.com>
2023-02-23 19:03:20 -0700

    Added the DB context, and the 2 reqd models

---
+++ Models/TaskEntryContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group2_3_Mission8.Models
+{
+    public class TaskEntryContext : DbContext
+    {
+        public TaskEntryContext(DbContextOptions<TaskEntryContext> options) : base(options)
+        {
+            // Leave blank for now
+        }
+
+        public DbSet<TaskFormResponse> Task { get; set; }
+        public DbSet<Category> Category { get; set; }
+
+    }
+}
---
+++ Models/TaskFormResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group2_3_Mission8.Models
+{
+    public class TaskFormResponse
+    {
+        [Key]
+        [Required]
+        public int TaskId { get; set; }
+        public string Task { get; set; } // Task Name
+        public DateTime DueDate { get; set; }
+        public int Quadrant { get; set; }
+        public bool Completed { get; set; }
+
+        // Build FK Relationship
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
